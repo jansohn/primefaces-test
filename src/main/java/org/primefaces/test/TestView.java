be75c78f77205b97f7f5373b5a92cf5b9aa2e9d8
jansohn <jansohn@users.noreply.github.com>
2025-02-19 13:05:06 +0100

    added draggable datatable rows with selectonemenu column

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,15 +1,20 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.primefaces.event.ReorderEvent;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.AjaxBehaviorEvent;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 import lombok.Data;
 
 @Data
@@ -22,6 +27,7 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     private List<TestObject> list;
+    private List<String> artists;
 
     @PostConstruct
     public void init() {
@@ -30,8 +36,29 @@ public class TestView implements Serializable {
                 new TestObject("Thriller", "Michael Jackson", 1982),
                 new TestObject("Back in Black", "AC/DC", 1980),
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
-        ));
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)));
+        artists = new ArrayList<>(Arrays.asList("Michael Jackson",
+                "AC/DC",
+                "Whitney Houston",
+                "Pink Floyd"));
     }
 
+    public void onRowReorder(ReorderEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Row Moved", "From: " + event.getFromIndex() + ", To:" + event.getToIndex());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        for (TestObject item : list) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public void actionArtistSelected(AjaxBehaviorEvent event) {
+        Object source = event.getSource();
+
+        if (source instanceof String) {
+            String artist = (String) source;
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Artist Selected", "New value: " + artist);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
 }
