53e3790dcee5a87ec559ad78ee5daff0ec44d13c
jansohn <jansohn@users.noreply.github.com>
2025-02-24 15:49:51 +0100

    added filterValue reproducer

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,15 +1,15 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 import lombok.Data;
 
 @Data
@@ -22,6 +22,10 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     private List<TestObject> list;
+    private List<TestObject> filteredList;
+    private List<String> artists;
+    private TestObject selectedObject;
+    private String filterArtist;
 
     @PostConstruct
     public void init() {
@@ -30,8 +34,17 @@ public class TestView implements Serializable {
                 new TestObject("Thriller", "Michael Jackson", 1982),
                 new TestObject("Back in Black", "AC/DC", 1980),
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
-        ));
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)));
+        artists = new ArrayList<>(Arrays.asList("Michael Jackson",
+                "AC/DC",
+                "Whitney Houston",
+                "Pink Floyd"));
     }
 
+    public void activateDefaultFilter() {
+        this.selectedObject = list.get(0);
+        this.filterArtist = "Michael Jackson";
+
+        // PrimeFaces.current().executeScript("PF('dataTableWidget').filter();");
+    }
 }
