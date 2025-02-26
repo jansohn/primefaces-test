eea8d24397f2b6afa643cfa36cba3b9a8f7370a9
jansohn <jansohn@users.noreply.github.com>
2025-02-26 15:44:42 +0100

    added filterValue reproducer workaround

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,15 +1,18 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.primefaces.component.datatable.DataTable;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 import lombok.Data;
 
 @Data
@@ -22,6 +25,10 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     private List<TestObject> list;
+    private List<TestObject> filteredList;
+    private List<String> artists;
+    private TestObject selectedObject;
+    private String filterArtist;
 
     @PostConstruct
     public void init() {
@@ -30,8 +37,20 @@ public class TestView implements Serializable {
                 new TestObject("Thriller", "Michael Jackson", 1982),
                 new TestObject("Back in Black", "AC/DC", 1980),
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
-        ));
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)));
+        artists = new ArrayList<>(Arrays.asList("Michael Jackson",
+                "AC/DC",
+                "Whitney Houston",
+                "Pink Floyd"));
     }
 
+    public void activateDefaultFilter() {
+        this.selectedObject = list.get(0);
+        this.filterArtist = "Michael Jackson";
+
+        // filter and sort maps do not get automatically updated so we initialize them again explicitly
+        DataTable dataTable = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent(":frmTest:dataTable");
+        dataTable.initFilterBy(FacesContext.getCurrentInstance());
+        dataTable.initSortBy(FacesContext.getCurrentInstance());
+    }
 }
