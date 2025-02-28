53bee081a8117c8bbeeb7d84bdc8daca0a53628f
jansohn <jansohn@users.noreply.github.com>
2025-02-28 13:50:53 +0100

    added default lazy model commandlink reproducer

--- src/main/java/org/primefaces/test/TestObject.java
+++ src/main/java/org/primefaces/test/TestObject.java
@@ -2,6 +2,7 @@ package org.primefaces.test;
 
 import java.io.Serializable;
 import java.util.UUID;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,12 +14,14 @@ public class TestObject implements Serializable {
     private String name;
     private String artist;
     private Integer released;
+    private String details;
 
-    public TestObject(String name, String artist, Integer released) {
+    public TestObject(String name, String artist, Integer released, String details) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.artist = artist;
         this.released = released;
+        this.details = details;
     }
 
 }
--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,15 +1,17 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.primefaces.model.DefaultLazyDataModel;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 import lombok.Data;
 
 @Data
@@ -21,17 +23,47 @@ public class TestView implements Serializable {
     private Integer integer;
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
+    private DefaultLazyDataModel<TestObject> lazyDataModel;
     private List<TestObject> list;
+    private List<TestObject> filteredList;
+    private List<String> artists;
+    private TestObject selectedObject;
+    private TestObject clickedTestObject;
 
     @PostConstruct
     public void init() {
         string = "Welcome to PrimeFaces!!!";
         list = new ArrayList<>(Arrays.asList(
-                new TestObject("Thriller", "Michael Jackson", 1982),
-                new TestObject("Back in Black", "AC/DC", 1980),
-                new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
-        ));
+                new TestObject("Thriller", "Michael Jackson", 1982, null),
+                new TestObject("Back in Black", "AC/DC", 1980, "AC/DC band details"),
+                new TestObject("The Bodyguard", "Whitney Houston", 1992, null),
+                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973, null)));
+        artists = new ArrayList<>(Arrays.asList("Michael Jackson",
+                "AC/DC",
+                "Whitney Houston",
+                "Pink Floyd"));
+
+        this.lazyDataModel = DefaultLazyDataModel.<TestObject>builder()
+                .valueSupplier((filterBy) -> {
+                    System.out.println("valueSupplier.get() called with filterBy size " + filterBy.size());
+                    return Arrays.asList(
+                            new TestObject("Thriller", "Michael Jackson", 1982, null),
+                            new TestObject("Back in Black", "AC/DC", 1980, "AC/DC band details"),
+                            new TestObject("The Bodyguard", "Whitney Houston", 1992, null),
+                            new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973, null));
+                })
+                .rowKeyField("id")
+                .build();
     }
 
+    public void onClickDetailsIcon(TestObject testObject) {
+        System.out.println("clicked object: " + testObject.toString());
+        this.clickedTestObject = testObject;
+    }
+
+    public String getDetailsMessage() {
+        if (this.clickedTestObject != null) { return this.clickedTestObject.toString(); }
+
+        return "";
+    }
 }
